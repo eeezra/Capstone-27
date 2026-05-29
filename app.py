c190e828e45639f4c3062e5edee63a26c2392aca
sariyyan <sariyyan.ikp@gmail.com>
2026-05-29 19:53:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -402,6 +402,11 @@ if "uploaded_image" not in st.session_state:
 # SIDEBAR
 # =========================================================
 
+st.markdown(
+    "<h1 style='color:red'>TEST HTML</h1>",
+    unsafe_allow_html=True
+)
+
 with st.sidebar:
 
     st.markdown("""
